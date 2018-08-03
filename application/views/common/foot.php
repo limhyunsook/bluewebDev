9e8d54bd8d3e00c015ac0da20c96e18d11db87df
NoruYakeyo <34639753+NoruYakeyo@users.noreply.github.com>
2018-08-03 16:12:25 +0900

    첫 커밋 이것이 개발자의 길
    mvc 패턴 배우기 step 1
    근데 뭔지 모르것다

---
+++ application/views/common/foot.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title>INIpayTX5.0 ��ҿ�û ������ ����</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+        <link rel="stylesheet" href="css/group.css" type="text/css">
+        <style>
+            body, tr, td {font-size:9pt; font-family:����,verdana; color:#433F37; line-height:19px;}
+            table, img {border:none}
+
+            /* Padding ******/ 
+            .pl_01 {padding:1 10 0 10; line-height:19px;}
+            .pl_03 {font-size:20pt; font-family:����,verdana; color:#FFFFFF; line-height:29px;}
+
+            /* Link ******/ 
+            .a:link  {font-size:9pt; color:#333333; text-decoration:none}
+            .a:visited { font-size:9pt; color:#333333; text-decoration:none}
+            .a:hover  {font-size:9pt; color:#0174CD; text-decoration:underline}
+
+            .txt_03a:link  {font-size: 8pt;line-height:18px;color:#333333; text-decoration:none}
+            .txt_03a:visited {font-size: 8pt;line-height:18px;color:#333333; text-decoration:none}
+            .txt_03a:hover  {font-size: 8pt;line-height:18px;color:#EC5900; text-decoration:underline}
+        </style>
+        <script language="JavaScript" type="text/JavaScript">
+            <!--
+            function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+            if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+            document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+            else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+            }
+            MM_reloadPage(true);
+
+            function MM_jumpMenu(targ,selObj,restore){ //v3.0
+            eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+            if (restore) selObj.selectedIndex=0;
+            }
+            //-->
+        </script>
+    </head>
+    
