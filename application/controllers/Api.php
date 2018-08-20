7ab3deaa4eb9c8a72f93d0b15cc93d88e1f159e2
blueweb1984 <41274405+blueweb1984@users.noreply.github.com>
2018-08-20 14:22:23 +0900

    FMVC ver1
    파일 MVC  패턴 작업

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2,32 +2,77 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 //core 에서 MY 상속하고 있음 -- MY_ 는 고정 픽스. 변경시에는 config.php 에서 변경 해야한다
-class Api extends MY_Controller 
+class Html extends MY_Controller 
 {
 
 	//생성자 구성
-	function __construct()	
+	public function __construct()	
 	{
 		parent::__construct();		
-		$this->load->helper("url");
 		
-	}	
-	
+		$this->load->helper('url'); //url 관련 거의 사용됨
+		$this->load->helper('form'); //form 관련 유틸. 거의 사용됨
+
+		$this->load->library('session');//세션 라이드러리 로드
+		
+		//$this->output->enable_profiler(true); //프로파일 확인 디버깅 용도로 좋음  사용하지 않는다면 false 혹은 주석
+	}
+
+
 	//컨트롤러 시작시에 무조건 참고 한다. 생성자 호출 이후 호출됨
-	
+	public function _remap($method)
+	{
+		$this->segs = $this->uri->segment_array();
+		$location = $this->session->userdata('location'); //세션을 검색해서 넣음
+		if(!$location) $location = "kr"; //없으면 kr 기준으로 불러온다
+		
+
+		//파일 기준으로 view를 가져온다 
+		//주소는 가짜 주소이며, 파일명 기준으로 주소를 불러온다
+		//echo $method;
+		
+		//$php = str_replace(".php", "", $method);
+		//$file = $method = str_replace(".html", "", $method);
+
+		//echo $method;
+		$filepath = APPPATH."/views/page/".$method;
+		//var_dump(file_exists($filepath));		
+        
+		if(file_exists($filepath)){
+			$main_data = array();			
+			$this->_header($location); //내부함수 해더 호출
+			$this->load->view("/page/$method",$main_data);
+			$this->_footer($location); //내부함수 푸터 호출
+		}else{
+			echo "404 ERROR";
+		}
+		
+	}
 
 	
 	public function index()
 	{
-		
+		echo "디폴트 함수. 만들지 않아도 디폴트로 생성 된다";
 	}
 
-	public function one_one_api()
-	{
-
-		$parameter = $this->input->post(null,true);
-		print_r($parameter);
+	public function test(){
+		$this->mytest(); //core 에서 불러옴
 	}
 
+
+
+
+	//내부에서만 사용 되는 헤더 함수
+	public function _header($location){
+		$data = array("location"=>$location);
+		$this->load->view("/$location/common/header",$data);
+	}
+
+	//내부에서만 사용 되는 푸터 함수
+	public function _footer($location){
+		$data = array("location"=>$location);
+		$this->load->view("/$location/common/footer",$data);
+	}
+	
 	
 }
---
+++ application/views/html/inc/top.php
@@ -0,0 +1,286 @@
+<!--========== topbanner ==========-->
+<!--div style="position: relative;max-width:1200px;margin:0 auto;" id="topBannerArea">
+    <a href="#" style="display:block;" target="_blank">
+        <img src="/html/assets/images/mobile_banner1.jpg" alt="페키지 배너">
+    </a>
+    <div style="position: absolute;bottom: 5px;right: 75px;">
+        <img src="/html/assets/images/banner_close.gif" alt="닫기" onclick="objTBC.closeTopBannerArea();">
+    </div>
+    <div style="position: absolute;bottom: 5px;right: 0;">
+        <img src="/html/assets/images/banner_close2.gif" alt="안보기" onclick="objTBC.closeTopBannerCookie();">
+    </div>
+</div-->
+ <!--========== topbanner ==========-->
+
+<!--
+<nav class="blue-navbar wrapper">
+	<div class="nav-table">				
+		<ul class="nav-middle">
+            <li class="t-1"><a href="/html/member/login.php">로그인</a></li>			
+            <li>|</li>
+			<li><a href="#">홈페이지 제작센터</a></li>			
+            <li>|</li>
+			<li><a href="#">블루 CGI</a></li>
+			<li>|</li>
+			<li><a href="#">블루 SMS</a></li>				
+            <li>|</li>
+			<li><a href="#">파트너</a></li>				
+		</ul>	
+	</div>
+</nav>
+
+
+<div class="nav-menu">
+	<div class="container nav-bottom wrapper">
+		<h2>
+			<a href="/html/index.php"><img src="/html/assets/images/blueweb_logo.png" alt="블루웹 로고"></a>
+		</h2>	
+		<ul>
+			<li>
+				<a href="/html/hosting/service.php">호스팅</a>
+				 <div>
+					<div>
+						<nav>											
+                            <h2>웹호스팅</h2>
+							<p>빠른 처리속도, 뛰어난 안정성, 신속한 업그레이드, PHP 프로그램 최적화 환경</p>							
+                            <a href="#">서비스 알아보기 <i class="fa fa-angle-right"></i></a>
+						</nav>		
+						<div class="wrapper">
+							<ul>
+								<h2><a href="#">웹호스팅</a></h2>
+								<li><a href="/html/hosting/service.php">리눅스 웹호스팅 <img src="/html/assets/images/icon_new.png" alt="new"></a></li>
+                                <li><a href="#">윈도우 웹호스팅</a></li>
+								<li><a href="#">제로XE 호스팅</a></li>		
+                                <li><a href="#">.NET 호스팅</a></li>
+                                <li><a href="#">리셀러 호스팅</a></li>
+							</ul>		
+							<ul>
+								<h2><a href="#">솔루션 지원</a></h2>
+								<li><a href="#">이미지 호스팅</a></li>
+								<li><a href="#">동영상 호스팅</a></li>
+							</ul>		
+							<ul>
+								<h2><a href="#">웹사이트 구축 지원</a></h2>
+								<li><a href="#">이전 대행</a></li>
+								<li><a href="#">간편제작 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+								<li><a href="#">SSL호스팅</a></li>
+							</ul>	
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a href="#">서버호스팅</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>서버호스팅</h2>
+							<p>대의 서버를 독립적으로 이용하실 수 있는 서비스</p>							
+                            <a href="#">서비스 알아보기 <i class="fa fa-angle-right"></i> </a>
+						</nav>		
+						<div class="wrapper">
+							<ul>
+								<h2><a href="#">서버호스팅</a></h2>
+								<li><a href="#">서버호스팅이란?</a></li>
+								<li><a href="#">블루웹의 장점</a></li>
+								<li><a href="#">서비스 설비</a></li>
+								<li><a href="#">네트워크</a></li>
+							</ul>									
+							<ul>
+								<h2><a href="#">코로케이션</a></h2>	
+								<li><a href="#">코로케이션 안내</a></li>
+								<li><a href="#">서버 코로케이션</a></li>
+								<li><a href="#">랙 코로케이션</a></li>
+								<li><a href="#">신청절차 <img src="/html/assets/images/icon_update.png" alt="update"></a></li>
+								<li><a href="#">서비스 신청</a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">매니지먼트 서비스</a></h2>	
+								<li><a href="#">매니지먼트란</a></li>
+								<li><a href="#">서비스종류/신청</a></li>
+								<li><a href="#">작업신청 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">부가서비스</a></h2>	
+								<li><a href="#">무료 기본지원 서비스</a></li>
+								<li><a href="#">유료 지원 서비스</a></li>
+							</ul>					
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a href="#">도메인</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>도메인</h2>
+							<p>도메인은 개인 또는 기업을 대표하는 브랜드이자 첫인상입니다.</p>							
+                            <a href="#">서비스알아보기 <i class="fa fa-angle-right"></i> </a>
+						</nav>		
+						<div class="wrapper">
+							<ul>
+								<h2><a href="#">도메인 등록</a></h2>
+								<li><a href="#">도메인 검색</a></li>
+								<li><a href="#">구매 대행</a></li>
+							</ul>					
+							<ul>
+								<h2><a href="#">기간이전 <img src="/html/assets/images/icon_update.png" alt="update"></a></h2>
+								<li><a href="#">기관연장</a></li>	
+							</ul>							
+							<ul>
+								<h2><a href="#">도메인 가치평가</a></h2>
+								<li><a href="#">도메인 정보</a></li>
+							</ul>	
+						</div>
+					</div>
+				</div>
+			</li>			
+			<li>
+				<a href="#">보안</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>보안</h2>
+							<p>사이버 보안 위협으로부터 실시간 대응하며 고객의 정보를 안전하게 보호합니다.</p>							
+                            <a href="#">서비스알아보기 <i class="fa fa-angle-right"></i> </a>
+						</nav>		
+						<div class="wrapper">
+							<ul>	
+								<h2><a href="#">보안관제</a></h2>
+								<li><a href="#">원격관제</a></li>
+								<li><a href="#">파견관제</a></li>
+							</ul>	
+							<ul>	
+								<h2><a href="#">컨설팅</a></h2>
+								<li><a href="#">모의해킹 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+								<li><a href="#">취약점진단</a></li>
+                                <li><a href="#">침해사고분석</a></li>
+							</ul>	
+							<ul>	
+								<h2><a href="#">보안상품</a></h2>
+								<li><a href="#">SSL</a></li>
+								<li><a href="#">방화벽</a></li>
+                                <li><a href="#">웹 방화벽</a></li>   
+                                <li><a href="#">이메일 보안 <img src="/html/assets/images/icon_update.png" alt="update"></a></li>
+                                <li><a href="#">DB 암호화</a></li>                        
+							</ul>	
+							<ul>	
+								<h2><a href="#">보안 솔루션</a></h2>
+								<li><a href="#">문서 중앙화</a></li>
+                                <li><a href="#">랜섬웨어 차단</a></li>
+                                <li><a href="#">개인정보보호</a></li>
+                                <li><a href="#">유해 사이트 차단</a></li>                        
+							</ul>																						
+						</div>
+					</div>
+				</div>
+			</li>
+            <li>
+				<a href="#">솔루션</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>솔루션</h2>
+							<p>홈페이지 DIY(직접 제작)부터 전문 제작의뢰까지, 고객에게 딱 맞는 맞춤형 홈페이지를 제공합니다.</p>							
+                            <a href="#">서비스알아보기 <i class="fa fa-angle-right"></i> </a>
+						</nav>		
+						<div class="wrapper">
+							<ul>	
+								<h2><a href="#">이비즈로</a></h2>
+								<li><a href="#">홈페이지자동구축</a></li>
+								<li><a href="#">신청하기</a></li>
+							</ul>	
+							<ul>	
+								<h2><a href="#">쿠킹엠</a></h2>
+								<li><a href="#">7일무료체험 <img src="/html/assets/images/icon_update.png" alt="update"></a></li>
+								<li><a href="#">신청하기</a></li>
+							</ul>	
+							<ul>	
+								<h2><a href="#">코참비즈</a></h2>
+								<li><a href="#">상공회의소 회원 무료</a></li>
+								<li><a href="#">신청하기 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+							</ul>	
+							<ul>	
+								<h2><a href="#">홈페이지제작센터</a></h2>
+								<li><a href="#">신청하기</a></li>
+							</ul>																						
+						</div>
+					</div>
+				 </div>
+			</li>
+			<li>
+				<a href="#">고객센터</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>고객센터</h2>							
+						</nav>		
+						<div class="wrapper">
+							<ul>
+								<h2><a href="#">빠른 메뉴</a></h2>
+								<li><a href="#">세금계산서/영수증 발급</a></li>
+								<li><a href="#">요금 납부/미납 조회</a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">공지 및 안내</a></h2>
+								<li><a href="#">공지사항</a></li>
+								<li><a href="#">작업공지</a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">이벤트 및 신규서비스</a></h2>
+								<li><a href="#">이벤트 <img src="/html/assets/images/icon_update.png" alt="update"></a></li>
+								<li><a href="#">신규 서비스</a></li>
+							</ul>							
+							<ul>
+								<h2><a href="#">상담 및 문의</a></h2>
+								<li><a href="#">자주 묻는 질문 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+								<li><a href="#">1:1질문과 답변</a></li>
+								<li><a href="#">문의메일 보내기</a></li>							
+							</ul>					
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a href="#">내서비스관리</a>
+				<div>
+					<div>
+						<nav>											
+                            <h2>내서비스관리</h2>							
+						</nav>		
+						<div class="wrapper">
+							<ul>
+								<h2><a href="#">요금관리</a></h2>
+								<li><a href="#">결제조회 <img src="/html/assets/images/icon_new.png" alt="new"></a></li>
+								<li><a href="#">호스팅 요금납부</a></li>
+								<li><a href="#">도메인 예치금 관리</a></li>
+								<li><a href="#">결제방법안내</a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">호스팅관리</a></h2>
+								<li><a href="#">호스팅 신청/결제 확인</a></li>
+								<li><a href="#">호스팅 정보변경</a></li>
+								<li><a href="#">호스팅 명의변경 <img src="/html/assets/images/icon_event.png" alt="event"></a></li>
+							</ul>
+							<ul>
+								<h2><a href="#">도메인관리</a></h2>
+								<li><a href="#">도메인 사용 현황 <img src="/html/assets/images/icon_update.png" alt="update"></a></li>
+								<li><a href="#">도메인 신청/결제 확인</a></li>
+								<li><a href="#">부가서비스 신청</a></li>
+								<li><a href="#">도메인 소유권 이전</a></li>							
+							</ul>
+							<ul>	
+								<h2><a href="#">나만의 혜택 관리</a></h2>
+                                <li><a href="#">회원정보 변경</a></li>
+								<li><a href="#">1:1 친절상담실</a></li>
+								<li><a href="#">이벤트 쿠폰</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</li>				
+		</ul>
+	</div>
+</div>
+-->
